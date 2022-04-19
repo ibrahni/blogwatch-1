75fa9446120e1fcab6bc8db5444e198369d39ab3
ibrahni <diihnini@gmail.com>
2022-04-19 05:52:37 +0000

    update prices test

--- src/test/java/com/baeldung/selenium/page/CoursePageUITest.java
+++ src/test/java/com/baeldung/selenium/page/CoursePageUITest.java
@@ -2,26 +2,22 @@ package com.baeldung.selenium.page;
 
 
 import static com.baeldung.common.ConsoleColors.magentaColordMessage;
+import static com.baeldung.common.ConsoleColors.redBoldMessage;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.openqa.selenium.WebElement;
 
-import com.baeldung.common.GlobalConstants;
 import com.baeldung.common.TestMetricsExtension;
 import com.baeldung.common.vo.CoursePurchaseLinksVO;
 import com.baeldung.selenium.common.BaseUISeleniumTest;
@@ -31,6 +27,7 @@ import io.restassured.config.RestAssuredConfig;
 
 @ExtendWith(TestMetricsExtension.class)
 public class CoursePageUITest extends BaseUISeleniumTest {
+    private static final String DEFAULT_PRICE = "0";
     private static final String PRICE_REGEX = "\\$\\d+";
     final Pattern pattern = Pattern.compile(PRICE_REGEX, Pattern.MULTILINE);
 
@@ -65,10 +62,6 @@ public class CoursePageUITest extends BaseUISeleniumTest {
     public void should_verify_courses_prices_with_teachable(String url, List<CoursePurchaseLinksVO.PurchaseLink> purchaseLinks) {
         logger.info(magentaColordMessage("checking prices for courses in page {} "), url);
 
-        final List<Executable> purchaseLinkDisplayedOnBlog = new ArrayList<>();
-        final List<Executable> priceExistsOnBlog = new ArrayList<>();
-        final List<Executable> priceDisplayedOnTeachable = new ArrayList<>();
-        final List<Executable> priceExistsOnTeachable = new ArrayList<>();
         final List<Executable> comparePrices = new ArrayList<>();
 
         purchaseLinks.forEach(purchaseLink -> {
@@ -77,55 +70,41 @@ public class CoursePageUITest extends BaseUISeleniumTest {
 
             logger.info(magentaColordMessage("checking prices for course [{}] in page {} "), purchaseLink.getLinkId(), url);
 
-            final boolean purchaseLinkDisplayed = page.containsById(purchaseLink.getLinkId());
+            try {
 
-            purchaseLinkDisplayedOnBlog.add(() -> assertTrue(purchaseLinkDisplayed, String.format("Couldn't find Purchase link with id: [%s] on %s", purchaseLink.getLinkId(), url)));
 
-            if(purchaseLinkDisplayed) {
-                final WebElement element = page.findById(purchaseLink.getLinkId());
-                final Matcher matcher = pattern.matcher(element.getText());
-                final boolean priceExistsOnBlogPage = matcher.find();
+                final String displayedPrice = getPriceFromText(page.findById(purchaseLink.getLinkId()).getText(), purchaseLink.getLinkId(), url);
+                logger.info("retrieving price {} from blog for {}", displayedPrice, purchaseLink.getLinkId());
 
-                priceExistsOnBlog.add(() -> assertTrue(priceExistsOnBlogPage, String.format(" Couldn't retrieve price for the following items [%s], in page [%s]", purchaseLink.getLinkId(), url)));
+                page.clickOnPurchaseButton(purchaseLink);
 
-                try {
-                    final String displayedPrice = matcher.group(0);
-                    logger.info("retrieving price {} from blog for {}", displayedPrice, purchaseLink.getLinkId());
+                final String priceOnTeachable = getPriceFromText(page.findTotalOnTeachable(), purchaseLink.getLinkId(), page.getUrl());
+                logger.info("retrieving price {} from teachable for {}", priceOnTeachable, purchaseLink.getLinkId());
 
-                    page.clickOnPurchaseButton(purchaseLink);
+                comparePrices.add(() -> Assertions.assertTrue(!displayedPrice.equals(DEFAULT_PRICE) && !priceOnTeachable.equals(DEFAULT_PRICE) && Objects.equals(displayedPrice, priceOnTeachable),
+                    String.format("Displayed price [%s] for element [%s] for course [%s] does not equal price [%s] on teachable page ", displayedPrice, url, purchaseLink.getLinkId(), priceOnTeachable)));
 
-                    final boolean priceExistOnTeachable = page.containsTotalOnTeachable();
-
-                    priceDisplayedOnTeachable.add(() -> assertTrue(priceExistOnTeachable, String.format("Couldn't find Course Total in teachable page %s ", page.getWebDriver()
-                        .getCurrentUrl())));
-
-                    if (priceExistOnTeachable) {
-                        final String totalDisplayOnTeachable = page.findTotalOnTeachable();
-                        final boolean containsPrice = pattern.matcher(totalDisplayOnTeachable)
-                            .find();
-                        priceExistsOnTeachable.add(() -> Assertions.assertTrue(containsPrice, String.format("Couldn't find price on Teachable %s ", page.getWebDriver()
-                            .getCurrentUrl())));
-                        if (containsPrice) {
-
-                            final String priceOnTeachable = matcher.group(0);
-                            logger.info("retrieving price {} from teachable for {}", priceOnTeachable, purchaseLink.getLinkId());
-
-                            comparePrices.add(() -> Assertions.assertEquals(displayedPrice, priceOnTeachable,
-                                String.format("Displayed price [%s] for element [%s] for course [%s] does not equal price [%s] on teachable page ", displayedPrice, url, purchaseLink.getLinkId(), priceOnTeachable)));
-                        }
-                    }
-                } catch (Exception ex) {
-                    logger.error(ex.getMessage());
-                }
+            } catch (Exception ex) {
+                logger.error("Exception occurred while comparing prices for [{}] in course page [{}] : {}", purchaseLink.getLinkId(), url, ex.getMessage());
             }
         });
 
-        assertAll(purchaseLinkDisplayedOnBlog);
-        assertAll(priceExistsOnBlog);
-        assertAll(priceDisplayedOnTeachable);
-        assertAll(priceExistsOnTeachable);
         assertAll(comparePrices);
 
+    }
+
+
+    private String getPriceFromText(String text, String linkId, String url){
+        final Matcher priceMatcher = pattern.matcher(text);
+
+        String displayedPrice = DEFAULT_PRICE;
+
+        if(priceMatcher.find()){
+            displayedPrice = priceMatcher.group(0);
+        }else{
+            logger.info(redBoldMessage("The price for {} link on {} page not found."), linkId, url);
+        }
+        return displayedPrice;
     }
 
 }
