45873887e1fd9119c70145a7c5aef9c88488f864
asif-anwar <asifanwar451@gmail.com>
2022-06-29 15:53:43 +0500

    small changes

--- src/test/java/com/baeldung/selenium/unittest/CommonUnitTest.java
+++ src/test/java/com/baeldung/selenium/unittest/CommonUnitTest.java
@@ -4,18 +4,23 @@ import static com.baeldung.common.Utils.replaceTutorialLocalPathWithHttpUrl;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.baeldung.common.GlobalConstants;
 
 import io.restassured.RestAssured;
 
 public class CommonUnitTest {
+    protected Logger logger = LoggerFactory.getLogger(getClass());
 
     @Test
     void givenAReadmeWithLocalSystemPath_whenConvertToHttpURL_itReturn200OK(){
         
         String localSystemPath="/var/lib/jenkins/tutorials-source-code/akka-streams/README.md";
         String httpUrl = replaceTutorialLocalPathWithHttpUrl(GlobalConstants.tutorialsRepoLocalPath, GlobalConstants.tutorialsRepoMasterPath).apply(localSystemPath);
+        
+        logger.info("URL to test: {}", httpUrl);
         
         assertTrue(RestAssured.given().get(httpUrl).getStatusCode() == 200);
         
