453f75381c24a3876c8ae9c7c86f8a20684606d2
Dan Geafer <gdan10b@gmail.com>
2022-07-22 13:59:29 +0300

    small refactor

--- src/test/java/com/baeldung/selenium/integrationtest/SeleniumIntegrationTest.java
+++ src/test/java/com/baeldung/selenium/integrationtest/SeleniumIntegrationTest.java
@@ -3,7 +3,6 @@ package com.baeldung.selenium.integrationtest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,7 +10,6 @@ import com.baeldung.common.TestMetricsExtension;
 import com.baeldung.selenium.common.BaseUISeleniumTest;
 import com.baeldung.site.EbookPageDriver;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(TestMetricsExtension.class)
 public class SeleniumIntegrationTest extends BaseUISeleniumTest {
 
